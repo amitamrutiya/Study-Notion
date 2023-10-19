ae215b4e2e2a8551bf2f533ef93f1f654a7d5acd
Amit Amrutiya <amitamrutiya2210@gmail.com>
2023-10-19 17:41:04 +0530

    add image upload utils

---
+++ utils/imageUploader.js
@@ -0,0 +1,21 @@
+const cloudinary = require("cloudinary").v2;
+
+exports.uploadImageToCloudinary = async (file, folder, height, quality) => {
+  try {
+    const options = { folder };
+
+    if (height) {
+      options.height = height;
+    }
+    if (quality) {
+      options.quality = quality;
+    }
+
+    options.resource_type = "auto";
+
+    return await cloudinary.uploader.upload(file.tempFilePath, options);
+  } catch (error) {
+    console.log("Error in uploading image to cloudinary: " + error);
+    throw error;
+  }
+};
