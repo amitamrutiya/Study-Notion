874b59fc541e6a6b148175b66a2e382b8b1876e9
Amit Amrutiya <amitamrutiya2210@gmail.com>
2023-10-19 12:02:35 +0530

    configure mongodb database with the help of mongoose

---
+++ config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+exports.connect = () => {
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("DB Connected Successfully");
+    })
+    .catch((err) => {
+      console.log("DB Connection Failed");
+      console.log(err);
+      process.exit(1);
+    });
+};
