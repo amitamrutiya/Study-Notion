aea0f7cbb84a4fa853794deb829b64d49b38c3e7
Amit Amrutiya <amitamrutiya2210@gmail.com>
2023-10-20 14:10:13 +0530

    delete account and get user account method on user controller

---
+++ controller/User.js
@@ -0,0 +1,91 @@
+const User = require("../models/User");
+const Profile = require("../models/Profile");
+const Course = require("../models/Course");
+//Delete Account
+exports.deleteUserAccount = async (req, res) => {
+  try {
+    // Get user id
+    const { id } = req.user;
+
+    // validation
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: "User id is required",
+      });
+    }
+
+    // find profile
+    const userDetails = await User.findById(id);
+    if (!userDetails) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    const profileId = userDetails.additionalDetails;
+    if (!profileId) {
+      return res.status(400).json({
+        success: false,
+        message: "Profile not found",
+      });
+    }
+
+    //if user is students then remove from enrolled courses
+    if (req.user.accountType === "student") {
+      await Course.updateMany(
+        { studentsEnrolled: id },
+        { $pull: { studentsEnrolled: id } }
+      );
+    }
+    // delete profile and account
+    await Profile.findByIdAndDelete(profileId);
+    await User.findByIdAndDelete(id);
+
+    // return response
+    return res.status(201).json({
+      success: true,
+      message: "Account deleted successfully",
+    });
+  } catch (error) {
+    console.log("Error in deleteAccount", error);
+    res.status(500).json({
+      success: false,
+      message: "Unable to delete Account",
+      error: error.message,
+    });
+  }
+};
+
+//Get User Details
+exports.getUserDetails = async (req, res) => {
+  try {
+    //get id
+    const { id } = req.user;
+
+    //validation and get user details
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: "User id is required",
+      });
+    }
+    const userDetails = await User.findById(id)
+      .populate("additionalDetails")
+      .exec();
+
+    //return response
+    return res.status(201).json({
+      success: true,
+      message: "User details fetched successfully",
+      data: userDetails,
+    });
+  } catch (error) {
+    console.log("Error in getUserDetails", error);
+    res.status(500).json({
+      success: false,
+      message: "Unable to fetch user details",
+      error: error.message,
+    });
+  }
+};
