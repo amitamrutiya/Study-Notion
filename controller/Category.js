91930295033a094e2f246e905324dc0f1673df1b
Amit Amrutiya <amitamrutiya2210@gmail.com>
2023-10-21 17:48:47 +0530

    implement categoryPageDetails method

--- controller/Category.js
+++ controller/Category.js
@@ -1,4 +1,5 @@
 const Category = require("../models/Category");
+const Course = require("../models/Course");
 
 //create category handler function
 exports.createCategory = async (req, res) => {
@@ -67,5 +68,66 @@ exports.showAllcategories = async (req, res) => {
       success: false,
       message: error.message,
     });
+  }
+};
+
+// get category page details
+exports.categoryPageDetails = async (req, res) => {
+  try {
+    // get categoryId
+    const { categoryId } = req.body;
+
+    // validate data
+    if (!categoryId) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide category id",
+      });
+    }
+
+    // get category details
+    const selectedCategory = await Category.findById(categoryId, { new: true })
+      .populate("courses")
+      .exec();
+
+    //validation
+    if (!selectedCategory) {
+      return res.status(400).json({
+        success: false,
+        message: "Data not found",
+      });
+    }
+
+    // get courses of different category
+    const otherCategories = await Category.find(
+      { _id: { $ne: categoryId } },
+      { new: true }
+    )
+      .populate("courses")
+      .exec();
+
+    // get top selling courses
+    const topSellingCourses = await Course.find({ status: "Published" })
+      .populate("category")
+      .populate("instructor")
+      .populate("ratingAndReviews")
+      .populate("studentsEnrolled")
+      .sort({ studentsEnrolled: -1 })
+      .limit(7)
+      .exec();
+
+    // send response
+    return res.status(200).json({
+      success: true,
+      message: "Category page details",
+      data: {
+        otherCategories,
+        selectedCategory,
+        topSellingCourses,
+      },
+    });
+  } catch (error) {
+    console.log("Error in categoryPageDetails");
+    return res.status(500).json({ success: false, message: error.message });
   }
 };
