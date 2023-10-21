672802f469f50e1de96dc418390cc6d84f861e71
Amit Amrutiya <amitamrutiya2210@gmail.com>
2023-10-21 16:54:11 +0530

    create rating and review controller and declare required methods

---
+++ controller/RatingAndReview.js
@@ -0,0 +1,12 @@
+const RatingAndReview = require("../models/RatingAndReview");
+const Course = require("../models/Course");
+const User = require("../models/User");
+
+// create Rating and review
+exports.createRating = async (req,res) => {}
+
+// get average rating and review
+exports.getAverageRating = async (req,res) => {}
+
+// get all rating and review
+exports.getAllRating = async (req,res) => {}
