dc509941d87f7656a70c107c07d7934f5ee16643
Amit Amrutiya <amitamrutiya2210@gmail.com>
2023-10-27 17:24:40 +0530

    asdf

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-slate-400">Hello world!</h1>
+    <h1 className="text-3xl font-bold underline bg-slate-400 border-r-emerald-50">
+      Hello world!
+    </h1>
   );
 }
