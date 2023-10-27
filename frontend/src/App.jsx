dd6c2c1defbd1ea0dea827075471992c07776875
Amit Amrutiya <amitamrutiya2210@gmail.com>
2023-10-27 20:42:15 +0530

    create home.jsx file and add homepage components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <div>
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,10 +5,10 @@ function Home() {
   return (
     <div>
       {/* Section 1 */}
-      <div>
+      <div className="relative mx-auto flex flex-col w-11/12 items-center text-white justify-between ">
         <Link to={"/signup"}>
-          <div>
-            <div>
+          <div className="group mt-16 p-1 mx-auto rounded-full bg-richblue-800 font-semibold text-richblack-200 transition-all duration-200 hover:scale-95 w-fit">
+            <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblue-900 ">
               <p>Become an Instructor</p>
               <FaArrowRight />
             </div>
