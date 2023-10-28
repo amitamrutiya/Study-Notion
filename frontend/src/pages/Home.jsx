7d722ce112643ee43d6426c7074616ae96f2ce23
Amit Amrutiya <amitamrutiya2210@gmail.com>
2023-10-28 10:16:44 +0530

    create section 1 in homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,120 @@
 import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa";
+import HighlightText from "../components/core/HomePage/HighlightText";
+import Banner from "../assets/Images/banner.mp4";
+// import Footer from "../components/common/Footer";
+import CTAButton from "../components/core/HomePage/Button";
+import CodeBlocks from "../components/core/HomePage/CodeBlocks";
+import ExploreMore from "../components/core/HomePage/ExploreMore";
+// import InstructorSection from "../components/core/HomePage/InstructorSection";
+// import LearningLanguageSection from "../components/core/HomePage/LearningLanguageSection";
+// import TimelineSection from "../components/core/HomePage/TimelineSection";
+// import ReviewSlider from "../components/common/ReviewSlider";
 
 function Home() {
   return (
     <div>
-      {/* Section 1 */}
-      <div className="relative mx-auto flex flex-col w-11/12 items-center text-white justify-between ">
+      {/*Section1  */}
+      <div className="relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center text-white justify-between gap-8">
         <Link to={"/signup"}>
-          <div className="group mt-16 p-1 mx-auto rounded-full bg-richblue-800 font-semibold text-richblack-200 transition-all duration-200 hover:scale-95 w-fit">
-            <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblue-900 ">
+          <div className="group mx-auto mt-16 w-fit rounded-full bg-richblack-800 p-1 font-bold text-richblack-200 drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] transition-all duration-200 hover:scale-95 hover:drop-shadow-none">
+            <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
               <p>Become an Instructor</p>
-              <FaArrowRight />
-            </div>
+              <FaArrowRight />{" "}
+            </div>{" "}
           </div>
         </Link>
+
+        <div className="text-center text-4xl font-semibold">
+          Empower Your Future with
+          <HighlightText text={"Coding Skills"} />{" "}
+          {/* here we passed "Coding Skills" text as props in component <Highlight> so we apply filter in that text part*/}
+        </div>
+
+        <div className="-mt-3 w-[90%] text-center text-lg font-semibold text-richblack-300">
+          With our online coding courses, you can learn at your own pace, from
+          anywhere in the world, and get access to a wealth of resources,
+          including hands-on projects, quizzes, and personalized feedback from
+          instructors.
+        </div>
+
+        <div className="flex flex-row gap-7 mt-8">
+          {" "}
+          {/* here we passed "Learn More" as a children in CTAButton component and here compnent is used because there are many button like this; */}
+          <CTAButton active={true} linkto={"/signup"}>
+            {" "}
+            Learn More{" "}
+          </CTAButton>{" "}
+          {/* here active is passed as props because if active is true then button is yellow otherwise black; */}
+          <CTAButton active={false} linkto={"/login"}>
+            {" "}
+            Book a Demo{" "}
+          </CTAButton>
+        </div>
+
+        <div className="mx-3 my-7 shadow-[10px_-5px_50px_-5px] shadow-blue-200">
+          <video
+            muted
+            loop
+            autoPlay
+            className="shadow-[20px_20px_rgba(255,255,255)]"
+          >
+            {" "}
+            <source src={Banner} type="video/mp4" />{" "}
+          </video>
+        </div>
+
+        <div>
+          <CodeBlocks
+            position={"lg:flex-row"}
+            heading={
+              <div className="text-4xl font-semibold">
+                Unlock Your <HighlightText text={"coding potential"} /> with our online courses
+              </div>
+            }
+            subheading={
+              "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+            }
+            ctabtn1={{
+              btnText: "Try it yourself",
+              linkto: "/signup",
+              active: true,
+            }}
+            ctabtn2={{
+              btnText: "Learn more",
+              linkto: "/login",
+              active: false,
+            }}
+            codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
+            codeColor={"text-yellow-25"}
+            backgroundGradient={<div className="codeblock1 absolute"></div>}
+          />
+        </div>
+
+        <div>
+          <CodeBlocks
+            position={"lg:flex-row-reverse"}
+            heading={
+              <div className="w-[100%] text-4xl font-semibold lg:w-[50%]">
+                Start <HighlightText text={"coding in seconds"} />
+              </div>
+            }
+            subheading={
+              "Go ahead, give it a try. Our hands-on learning environment means you'll be writing real code from your very first lesson."
+            }
+            ctabtn1={{
+              btnText: "Continue Lesson",
+              linkto: "/signup",
+              active: true,
+            }}
+            ctabtn2={{ btnText: "Learn More", linkto: "/login", active: false }}
+            codeblock={`import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`}
+            codeColor={"text-white"}
+            backgroundGradient={<div className="codeblock2 absolute"></div>}
+          />
+        </div>
+
+        <ExploreMore />
       </div>
       {/* Section 2 */}
 
