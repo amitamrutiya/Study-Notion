c2ceec2aa0f35404babf3103b94721c9b7829da5
Amit Amrutiya <amitamrutiya2210@gmail.com>
2023-10-28 12:15:56 +0530

    add new data into homepage-explore

--- frontend/src/data/homepage-explore.js
+++ frontend/src/data/homepage-explore.js
@@ -1,117 +1,132 @@
 export const HomePageExplore = [
-    {
-        tag: 'Free',
-        courses : [
-            {
-                heading : "Learn HTML",
-                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
-                level : 'Beginner',
-                lessionNumber : 6
-            },
-            {
-                heading : "Learn CSS",
-                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
-                level : 'Beginner',
-                lessionNumber : 6
-            },
-            {
-                heading : "Responsive Web design",
-                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
-                level : 'Beginner',
-                lessionNumber : 6
-            },
-        ]
-    },
-    {
-        tag: 'New to coding',
-        courses : [
-            {
-                heading : "HTML",
-                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
-                level : 'Beginner',
-                lessionNumber : 6
-            },
-            {
-                heading : "CSS",
-                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
-                level : 'Beginner',
-                lessionNumber : 6
-            },
-            {
-                heading : "Responsive ",
-                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
-                level : 'Beginner',
-                lessionNumber : 6
-            },
-        ]
-    },
-    {
-        tag: 'Most popular',
-        courses : [
-            {
-                heading : "Java",
-                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
-                level : 'Beginner',
-                lessionNumber : 6
-            },
-            {
-                heading : "Python",
-                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
-                level : 'Beginner',
-                lessionNumber : 6
-            },
-            {
-                heading : "SCSS",
-                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
-                level : 'Beginner',
-                lessionNumber : 6
-            },
-        ]
-    },
-    {
-        tag: 'Skills paths',
-        courses : [
-            {
-                heading : "Flask",
-                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
-                level : 'Beginner',
-                lessionNumber : 6
-            },
-            {
-                heading : "Django",
-                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
-                level : 'Beginner',
-                lessionNumber : 6
-            },
-            {
-                heading : "Fast API",
-                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
-                level : 'Beginner',
-                lessionNumber : 6
-            },
-        ]
-    },
-    {
-        tag: 'Career paths',
-        courses : [
-            {
-                heading : "Next.js",
-                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
-                level : 'Beginner',
-                lessionNumber : 6
-            },
-            {
-                heading : "Nuxt.js",
-                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
-                level : 'Beginner',
-                lessionNumber : 6
-            },
-            {
-                heading : "Sanity",
-                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
-                level : 'Beginner',
-                lessionNumber : 6
-            },
-        ]
-    },
-]
+  {
+    tag: "Free",
+    courses: [
+      {
+        heading: "Learn HTML",
+        description:
+          "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+        level: "Beginner",
+        lessonNumber: 6,
+      },
+      {
+        heading: "Learn CSS",
+        description:
+          "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+        level: "Intermediate",
+        lessonNumber: 8,
+      },
+      {
+        heading: "Responsive Web design",
+        description:
+          "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+        level: "Intermediate",
+        lessonNumber: 8,
+      },
+    ],
+  },
+  {
+    tag: "New to coding",
+    courses: [
+      {
+        heading: "HTML",
+        description:
+          "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+        level: "Beginner",
+        lessonNumber: 6,
+      },
+      {
+        heading: "CSS",
+        description:
+          "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+        level: "Intermediate",
+        lessonNumber: 8,
+      },
+      {
+        heading: "Responsive",
+        description:
+          "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+        level: "Intermediate",
+        lessonNumber: 8,
+      },
+    ],
+  },
+  {
+    tag: "Most popular",
+    courses: [
+      {
+        heading: "Java",
+        description:
+          "This course covers the basic concepts of Java programming, including variables, data types, control structures, and more.",
+        level: "Beginner",
+        lessonNumber: 10,
+      },
+      {
+        heading: "Python",
+        description:
+          "This course covers the basic concepts of Python programming, including variables, data types, control structures, and more.",
+        level: "Beginner",
+        lessonNumber: 10,
+      },
+      {
+        heading: "SCSS",
+        description:
+          "This course teaches advanced techniques for using SCSS to create responsive web designs.",
+        level: "Intermediate",
+        lessonNumber: 8,
+      },
+    ],
+  },
+  {
+    tag: "Skills paths",
+    courses: [
+      {
+        heading: "Flask",
+        description:
+          "This course covers the basic concepts of Flask web development, including routing, templates, and more.",
+        level: "Intermediate",
+        lessonNumber: 8,
+      },
+      {
+        heading: "Django",
+        description:
+          "This course covers the basic concepts of Django web development, including models, views, and templates.",
+        level: "Intermediate",
+        lessonNumber: 8,
+      },
+      {
+        heading: "Fast API",
+        description:
+          "This course covers the basic concepts of Fast API web development, including routing, models, and more.",
+        level: "Intermediate",
+        lessonNumber: 8,
+      },
+    ],
+  },
+  {
+    tag: "Career paths",
+    courses: [
+      {
+        heading: "Next.js",
+        description:
+          "This course covers the basic concepts of Next.js web development, including server-side rendering, routing, and more. It is a great choice for building scalable web applications.",
+        level: "Intermediate",
+        lessonNumber: 8,
+      },
+      {
+        heading: "Nuxt.js",
+        description:
+          "This course covers the basic concepts of Nuxt.js web development, including server-side rendering, routing, and more. It is a great choice for building server-rendered Vue.js applications.",
+        level: "Intermediate",
+        lessonNumber: 8,
+      },
+      {
+        heading: "Sanity",
+        description:
+          "This course covers the basic concepts of Sanity CMS development, including schema design, content modeling, and more. It is a great choice for building modern content-driven websites.",
+        level: "Intermediate",
+        lessonNumber: 8,
+      },
+    ],
+  },
+];
