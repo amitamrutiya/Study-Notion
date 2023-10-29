5f998b841bbe2f2b3887ba58649722ee45d6d3bb
Amit Amrutiya <amitamrutiya2210@gmail.com>
2023-10-29 20:18:29 +0530

    add some routes in app.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Navbar from "./components/common/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </div>
   );
