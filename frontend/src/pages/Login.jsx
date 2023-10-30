ef6bbdd44aa88f083e8055281b5260216f13d585
Amit Amrutiya <amitamrutiya2210@gmail.com>
2023-10-30 13:34:20 +0530

    format login and signup page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,16 @@
-import loginImg from "../assets/Images/login.webp"
-import Template from "../components/core/Auth/Template"
+import loginImg from "../assets/Images/login.webp";
+import Template from "../components/core/Auth/Template";
 
-function Login({setIsLoggedIn}){
+function Login({ setIsLoggedIn }) {
   return (
+    <Template
+      title="Welcome Back"
+      description1="Build skills for today, tomorrow, and beyond."
+      description2="Education to future-proof your career."
+      image={loginImg}
+      formtype="login"
+    />
+  );
+}
 
-    <Template title="Welcome Back"  description1="Build skills for today, tomorrow, and beyond." description2="Education to future-proof your career."   image = {loginImg} formtype = "login"/>
-
-  )}
-
-export default Login
+export default Login;
