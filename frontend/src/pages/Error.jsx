d60857ef7820b92008fe83913675f26ad47c2da3
Amit Amrutiya <amitamrutiya2210@gmail.com>
2023-10-30 13:33:17 +0530

    created Error routes if we go into invalid routes

---
+++ frontend/src/pages/Error.jsx
@@ -0,0 +1,10 @@
+const Error = () => {
+  return (
+    <div className="text-3xl text-white mx-auto my-auto">
+      {" "}
+      Error - 404 Not found{" "}
+    </div>
+  );
+};
+
+export default Error;
