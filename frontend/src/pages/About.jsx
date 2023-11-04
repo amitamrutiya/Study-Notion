50fa798578bacc530c413ee27b787038fecb2b49
Amit Amrutiya <amitamrutiya2210@gmail.com>
2023-11-04 20:46:34 +0530

    mention contact us form inside about us page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -3,7 +3,7 @@ import BannerImage1 from "../assets/Images/aboutus1.webp";
 import BannerImage2 from "../assets/Images/aboutus2.webp";
 import BannerImage3 from "../assets/Images/aboutus3.webp";
 
-// import ContactFormSection from "../components/core/AboutPage/ContactFormSection.js";
+import ContactFormSection from "../components/core/AboutPage/ContactFormSection.jsx";
 import LearningGrid from "../components/core/AboutPage/LearningGrid";
 import Quote from "../components/core/AboutPage/Quote";
 import StatsComponenet from "../components/core/AboutPage/Stats";
@@ -52,7 +52,7 @@ const About = () => {
             <div className="my-24 flex lg:w-[50%] flex-col gap-10">
               <h1 className="bg-gradient-to-br from-[#833AB4] via-[#FD1D1D] to-[#FCB045] bg-clip-text text-4xl font-semibold text-transparent lg:w-[70%] ">
                 {" "}
-                Our Founding Story{" "}
+                Our{" "}
               </h1>
               <p className="text-base font-medium text-richblack-300 lg:w-[95%]">
                 {" "}
@@ -121,7 +121,7 @@ const About = () => {
       <StatsComponenet /> {/* it shows stats (5k studend , 10+ courses) */}
       <section className="mx-auto mt-20 flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-white">
         <LearningGrid />
-        {/* <ContactFormSection /> */}
+        <ContactFormSection />
       </section>
       <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
         <h1 className="text-center text-4xl font-semibold mt-8">
@@ -130,7 +130,7 @@ const About = () => {
         </h1>
         {/* <ReviewSlider /> */}
       </div>
-      <Footer /> {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
