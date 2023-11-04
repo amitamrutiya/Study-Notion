41cd9675ecbc1d761bf455c2a5bd4f1828d9ccb3
Amit Amrutiya <amitamrutiya2210@gmail.com>
2023-11-04 23:58:29 +0530

    create profile drop down menu for redirect to my dashboard page

---
+++ frontend/src/utils/dateFormatter.js
@@ -0,0 +1,10 @@
+export const formattedDate = (date) => {
+    return new Date(date).toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    })
+  }
+
+
+  
