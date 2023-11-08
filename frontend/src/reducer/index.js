22868894be662dcdd15cfa0235d91fc08f8ad7ca
Amit Amrutiya <amitamrutiya2210@gmail.com>
2023-11-08 21:57:42 +0530

    did changes on formating

--- frontend/src/reducer/index.js
+++ frontend/src/reducer/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
-
 import authReducer from "../slices/authSlice.js";
 import profileReducer from "../slices/profileSlice.js";
 import cartReducer from "../slices/cartSlice.js";
 import courseReducer from "../slices/courseSlice.js";
 import viewCourseReducer from "../slices/viewCourseSlice.js";
-
 
 const rootReducer = combineReducers({
   // combining all reducer;
