2c3fc77f80158d23b883fbf78324cc5d1ce57381
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-02-13 17:01:09 +0530

    fix eslint failed issue

--- backend/test/utils/secToDuration.test.js
+++ backend/test/utils/secToDuration.test.js
@@ -1,15 +1,15 @@
-import convertSecondsToDuration from "../../utils/secToDuration";
+import convertSecondsToDuration from "../../utils/secToDuration"
 
 describe("convertSecondsToDuration", () => {
   it("should correctly convert seconds to hours and minutes", () => {
-    expect(convertSecondsToDuration(3661)).toBe("1h 1m");
-  });
+    expect(convertSecondsToDuration(3661)).toBe("1h 1m")
+  })
 
   it("should correctly convert seconds to minutes and seconds", () => {
-    expect(convertSecondsToDuration(61)).toBe("1m 1s");
-  });
+    expect(convertSecondsToDuration(61)).toBe("1m 1s")
+  })
 
   it("should correctly convert seconds to seconds", () => {
-    expect(convertSecondsToDuration(1)).toBe("1s");
-  });
-});
+    expect(convertSecondsToDuration(1)).toBe("1s")
+  })
+})
--- frontend/src/test/utils/avgRating.test.js
+++ frontend/src/test/utils/avgRating.test.js
@@ -1,9 +1,9 @@
-import GetAvgRating from "../../utils/avgRating";
+import GetAvgRating from "../../utils/avgRating"
 
 describe("GetAvgRating", () => {
   it("should return 0 when the array is empty", () => {
-    expect(GetAvgRating([])).toBe(0);
-  });
+    expect(GetAvgRating([])).toBe(0)
+  })
 
   it("should correctly calculate the average rating", () => {
     const ratings = [
@@ -12,12 +12,12 @@ describe("GetAvgRating", () => {
       { rating: 3 },
       { rating: 2 },
       { rating: 1 },
-    ];
-    expect(GetAvgRating(ratings)).toBe(3);
-  });
+    ]
+    expect(GetAvgRating(ratings)).toBe(3)
+  })
 
   it("should correctly round the average rating to one decimal place", () => {
-    const ratings = [{ rating: 5 }, { rating: 4 }, { rating: 4 }];
-    expect(GetAvgRating(ratings)).toBe(4.3);
-  });
-});
+    const ratings = [{ rating: 5 }, { rating: 4 }, { rating: 4 }]
+    expect(GetAvgRating(ratings)).toBe(4.3)
+  })
+})
